3d88a8662558af39bc94cc019ab2741e6237b184
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-10-28 21:00:33 -0600

    (Add):Initial config [src]

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("server works!");
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+// Midlewares
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', {title: "Node Website"});
+})
+
+// Static files
+
+// Listening the server
+app.listen(app.get('port'), _=> {
+    console.log('server on port', app.get('port'));
+});
